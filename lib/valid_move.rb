d4bd5e841b4fa76bcc8b0871d79266cd8185b85a
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-02-25 19:08:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if index > board.length
     false 
     
-  elsif position_taken?(board,index) == false
+  elsif position_taken?(board,index) == true
     false
     
   else
